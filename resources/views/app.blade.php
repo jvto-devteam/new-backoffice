6043ffc52b5fa933ea9a20ba98bfc19eacde4ea8
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2024-12-03 14:50:10 +0700

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="New Back Office JVTO" />
+        <meta property="og:description" content="Welcome to New Back Office JVTO" />
+        <meta property="og:url" content="https://new-backoffice.javavolcano-touroperator.com/" />
+        <meta property="og:image" content="https://javavolcano-touroperator.com/assets/img/download.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="627" />
+        <link rel="manifest" href="/manifest.json">        
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 

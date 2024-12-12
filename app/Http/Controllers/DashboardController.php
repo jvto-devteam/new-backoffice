16a008dfc485fa7a6a7e326113c507cc6610b1da
Dev id <davidsetya24@gmail.com>
2024-12-12 11:02:45 +0700

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,7 +15,7 @@ class DashboardController extends Controller
         $data['change_total_boking'] = $data['total_booking'] - $data['total_booking_last_month'];
 
         $data['total_booking_complete'] = Booking::where('travel_date_start','like','%'.date('Y-m').'%')->where('travel_date_end','<',date('Y-m-d'))->where('status','booked')->count();
-        $data['total_booking_on_going'] = Booking::where('travel_date_start','like','%'.date('Y-m').'%')->where('travel_date_start','>=',date('Y-m-d'))->where('travel_date_end','<=',date('Y-m-d'))->where('status','booked')->count();
+        $data['total_booking_on_going'] = Booking::where('travel_date_start','like','%'.date('Y-m').'%')->where('travel_date_start','<=',date('Y-m-d'))->where('travel_date_end','>=',date('Y-m-d'))->where('status','booked')->count();
 
         $data['booking'] = Booking::with(['bookingPayment.paymentMethod','bookingCategory', 'user.country','user.discount', 'agent', 'bookingDetail.package.duration', 'bookCar.car.garage', 'guideDriver.person', 'bookingItinerary.bookHotel.hotel', 'bookingItinerary.bookHotel.bookRoom.roomHotel.hotel.area','bookingItinerary.activityStart.destination'])->where('travel_date_start', '>=',date('Y-m-d'))->where('status', 'booked')->orderBy('travel_date_start','asc')->limit(5)->get();
         return Inertia::render('Dashboard2',['data' => $data]);

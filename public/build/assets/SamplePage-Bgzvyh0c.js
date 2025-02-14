edd136faf28c116b9234c9e4763037a7d27d0fdc
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2025-02-14 14:35:57 +0700

    update

--- public/build/assets/SamplePage-Bgzvyh0c.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-nSXzh5s4.js";import{M as a}from"./Main-ZtVh2slU.js";function e(){return r.jsx(a,{children:"Halo"})}export{e as default};
---
+++ public/build/assets/SamplePage-CPXESVlJ.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-ChyBX7v_.js";import{M as a}from"./Main-DMYvM3MX.js";function e(){return r.jsx(a,{children:"Halo"})}export{e as default};

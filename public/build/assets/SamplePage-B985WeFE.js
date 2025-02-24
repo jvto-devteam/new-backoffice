71ac07fff1cee1f2f5a66b811bc4f5ae1cb997ad
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2025-02-24 11:40:45 +0700

    update

---
+++ public/build/assets/SamplePage-B985WeFE.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-D-54VVGu.js";import{M as a}from"./Main-qPtGL152.js";function e(){return r.jsx(a,{children:"Halo"})}export{e as default};
--- resources/js/Pages/Finance/EditExpenseManager.tsx
+++ resources/js/Pages/Finance/EditExpenseManager.tsx
@@ -251,17 +251,20 @@ const ExpenseTable = ({ items, onPayLaterChange, onEdit,onDelete  }) => {
                     )}
                   </td>
                   <td className="px-4 py-3">
-                    <div className="flex justify-center">
-                      <button
-                        onClick={() => onDelete(index)}
-                        className="text-red-500 hover:text-red-700"
-                        title="Delete item"
-                      >
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                          <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm4 0a1 1 0 012 0v6a1 1 0 11-2 0V8z" clipRule="evenodd" />
-                        </svg>
-                      </button>
-                    </div>
+                    {item.category !== 'Accommodation' && (
+                      <div className="flex justify-center">
+                        <button
+                          onClick={() => onDelete(index)}
+                          className="text-red-500 hover:text-red-700"
+                          title="Delete item"
+                        >
+                          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm4 0a1 1 0 012 0v6a1 1 0 11-2 0V8z" clipRule="evenodd" />
+                          </svg>
+                        </button>
+                      </div>
+
+                    )}
                   </td>
                 </tr>
               );

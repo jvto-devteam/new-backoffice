ed500eb94d0d85778f969aa8620f5f245e8a9cc3
Inant Kharisma <inant.kharisma@gmail.com>
2025-03-05 12:34:20 +0700

    Update CorsServiceProvider.php

--- app/Providers/CorsServiceProvider.php
+++ app/Providers/CorsServiceProvider.php
@@ -3,18 +3,22 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Contracts\Http\Kernel;
-use App\Middleware\CorsMiddleware;
 
 class CorsServiceProvider extends ServiceProvider
 {
+    /**
+     * Register services.
+     */
     public function register(): void
     {
-        $this->app->singleton(CorsMiddleware::class);
+        //
     }
 
-    public function boot(Kernel $kernel): void
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
     {
-        $kernel->pushMiddleware(CorsMiddleware::class);
+        //
     }
 }

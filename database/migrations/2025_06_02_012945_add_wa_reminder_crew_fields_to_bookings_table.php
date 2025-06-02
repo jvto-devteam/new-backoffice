acef22c7234196fc8c88a62239cd2d8785620657
Dev id <davidsetya24@gmail.com>
2025-06-02 08:33:30 +0700

    Create 2025_06_02_012945_add_wa_reminder_crew_fields_to_bookings_table.php

---
+++ database/migrations/2025_06_02_012945_add_wa_reminder_crew_fields_to_bookings_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->enum('wa_status_reminder_crew', ['0', '1'])->default('0')->after('wa_schedule_trip_media_crew');
+            $table->dateTime('wa_schedule_reminder_crew')->nullable()->after('wa_status_reminder_crew');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->dropColumn(['wa_status_reminder_crew', 'wa_schedule_reminder_crew']);
+        });
+    }
+};

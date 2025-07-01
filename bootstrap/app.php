edff60c4b5d461e30a3c87ea1b8ecb065bfc0ecd
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2025-07-01 12:33:08 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,21 +14,17 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         // Daftarkan CorsMiddleware untuk grup 'web'
         $middleware->web(append: [
-            \App\Http\Middleware\CorsMiddleware::class, // <-- TAMBAHKAN INI
             \App\Http\Middleware\HandleInertiaRequests::class,
+            \App\Http\Middleware\CorsMiddleware::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
         // Daftarkan CorsMiddleware untuk grup 'api'
         $middleware->api(prepend: [
-            \App\Http\Middleware\CorsMiddleware::class, // <-- TAMBAHKAN INI
+            \App\Http\Middleware\CorsMiddleware::class,
         ]);
 
-        // Pindahkan konfigurasi VerifyCsrfToken ke sini agar lebih rapi
-        $middleware->preventRequestsWhenReadonly(except: [
-            // URI yang dikecualikan dari CSRF bisa ditambahkan di sini jika perlu
-        ]);
-
+        // Konfigurasi pengecualian untuk CSRF Token
         $middleware->validateCsrfTokens(except: [
             '/twt-extractor',
             '/third-party/webhook/watzap',

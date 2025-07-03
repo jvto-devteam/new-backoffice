684fb6f9e6b007ee2f49f5fd5b6555d163df0a23
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2025-07-03 17:16:46 +0700

    update

---
+++ public/build/assets/BookingList-Bj-GeJCK.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-_GfKT43w.js";import{M as i}from"./Main-DqSI4fRF.js";function n(){return r.jsx(i,{children:r.jsx("h1",{children:"CRM"})})}export{n as default};
--- public/build/assets/BookingList-Dq1f28rY.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-VMbjVQ6I.js";import{M as i}from"./Main-DJoHjG-y.js";function n(){return r.jsx(i,{children:r.jsx("h1",{children:"CRM"})})}export{n as default};

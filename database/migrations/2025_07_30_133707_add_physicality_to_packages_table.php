d751f65bdb0c4f7baa6e690c83f05c9bd1a7bc53
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2025-07-30 13:38:13 +0700

    Create 2025_07_30_133707_add_physicality_to_packages_table.php

---
+++ database/migrations/2025_07_30_133707_add_physicality_to_packages_table.php
@@ -0,0 +1,20 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('packages', function (Blueprint $table) {
+            $table->text('physicality')->nullable()->after('ideal_for');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('packages', function (Blueprint $table) {
+            $table->dropColumn('physicality');
+        });
+    }
+};

e9a7a0d362f75d48ab1582f3403413625a576321
Inant Kharisma <inant.kharisma@gmail.com>
2026-05-23 01:09:42 +0700

    update booking overview

--- public/build/assets/BookingList-QPR_PMnH.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-BplHalPb.js";import{M as i}from"./Main-CdXA47VH.js";function n(){return r.jsx(i,{children:r.jsx("h1",{children:"CRM"})})}export{n as default};
--- public/build/assets/SamplePage--p6WXxDw.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-BplHalPb.js";import{M as a}from"./Main-CdXA47VH.js";function e(){return r.jsx(a,{children:"Halo"})}export{e as default};

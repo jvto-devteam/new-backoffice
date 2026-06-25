9f3d4ad4fdd5353798b44110cb4a090aca6319a3
Inant Kharisma <inant.kharisma@gmail.com>
2026-06-25 11:28:24 +0700

    udpate booking revenue

--- public/build/assets/BookingList-Dn0XWdQU.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-dlmFT115.js";import{M as i}from"./Main-Du7p9O6U.js";function n(){return r.jsx(i,{children:r.jsx("h1",{children:"CRM"})})}export{n as default};
---
+++ public/build/assets/SamplePage-CfdCMcZr.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-Dma1aJkh.js";import{M as a}from"./Main-CDggCt8T.js";function e(){return r.jsx(a,{children:"Halo"})}export{e as default};
